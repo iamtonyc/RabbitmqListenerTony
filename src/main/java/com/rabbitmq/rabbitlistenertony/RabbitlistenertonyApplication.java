a71968f15fbd21634187bbb7e797f15b4a0a3b84
Tony Cheung <iamtonycheung@gmail.com>
2019-01-31 20:57:51 +0800

    first commit

---
+++ src/main/java/com/rabbitmq/rabbitlistenertony/RabbitlistenertonyApplication.java
@@ -0,0 +1,14 @@
+package com.rabbitmq.rabbitlistenertony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitlistenertonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitlistenertonyApplication.class, args);
+	}
+
+}
+
